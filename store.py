91c424bc46399362084b2b463b5e88b51bfbb838
Kirill Borisov <lensvol@gmail.com>
2013-08-06 00:27:01 +0400

    РЈР±СЂР°РЅ Р·Р°Р±С‹С‚С‹Р№ РєРѕРґ СЃСѓС‰РЅРѕСЃС‚РµР№ РёР· РѕСЃРЅРѕРІРЅРѕРіРѕ С„Р°Р№Р»Р°.

--- store.py
+++ store.py
@@ -4,49 +4,14 @@ import datetime
 import json
 import sys
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, create_engine, Time
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 
-Base = declarative_base()
+from entities import Train, Ticket, Base
+
 engine = create_engine('sqlite:///:memory:', echo=False)
 Session = sessionmaker(bind=engine)
-
-
-class Train(Base):
-    __tablename__ = 'trains'
-    id = Column(Integer, primary_key=True)
-    
-    name = Column(String)
-    departure = Column(Time)
-    start_point = Column(String)
-    end_point = Column(String)
-
-    tickets = relationship('Ticket', backref='train')
-
-    def __init__(self, name, departure, start, end):
-         self.name = name
-         self.departure = departure
-         self.start_point = start
-         self.end_point = end
-
-    def __repr__(self):
-        return '<%s(%r, %r)>' % (self.__class__.__name__, self.name, self.departure)
-
-
-class Ticket(Base):
-    __tablename__ = 'tickets'
-    electronic_id = Column(Integer, primary_key=True)
-
-    train_id = Column(Integer, ForeignKey('trains.id'))
-    cost = Column(Float)
-
-    def __init__(self, train, electronic_id, cost):
-        self.electronic_id = electronic_id
-        self.cost = cost
-        train.tickets.append(self)
-
 
 Base.metadata.create_all(engine)
 
