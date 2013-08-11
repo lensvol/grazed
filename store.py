ddc5b8bd7378701ff164f4fef7a48a244ce58c61
Kirill Borisov <lensvol@gmail.com>
2013-08-11 20:35:45 +0400

    РђРґСЂРµСЃ С…СЂР°РЅРёР»РёС‰Р° РґР°РЅРЅС‹С… Р±РµСЂРµС‚СЃСЏ РёР· РєРѕРЅС„РёРіР°, РѕС‚РєР°С‚С‹РІР°СЏСЃСЊ РЅР° SQLite РІ РїР°РјСЏС‚Рё РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ.\

--- store.py
+++ store.py
@@ -8,15 +8,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 
+from config import Config
 from entities import Train, Ticket, Base
 
-engine = create_engine('sqlite:///:memory:', echo=False)
+cfg = Config('grazed.conf')
+STORE_DBURI = cfg.get_value('store', 'dburi')
+
+engine = create_engine(STORE_DBURI or 'sqlite:///:memory:', echo=False)
 Session = sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     session = Session()
+    new_tickets = 0
 
     with open(sys.argv[1], 'r') as tickets_dump:
         tickets = json.loads(tickets_dump.read())
@@ -39,9 +44,10 @@ if __name__ == '__main__':
                                 .filter(Ticket.electronic_id == ticket['electronic_id']).one()
             except NoResultFound:
                 ticket = Ticket(train, ticket['electronic_id'], ticket['cost'])
+                new_tickets += 1
                 session.add(ticket)
 
         session.commit()
 
-        print 'Total: %i tickets for %i trains' % (session.query(Ticket).count(),
-                                                   session.query(Train).count())
+        print 'Total: %i tickets for %i trains (%i new)' % (session.query(Ticket).count(),
+                                                   session.query(Train).count(), new_tickets)
