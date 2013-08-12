2e24e40f12628ba5a28937f3345b16bd842f8178
Kirill Borisov <lensvol@gmail.com>
2013-08-12 17:19:35 +0400

    РћС‚СЃС‹Р»РєР° РїРѕ РЎРњРЎ РґР°РЅРЅС‹С… Рѕ РєСѓРїР»РµРЅРЅС‹С… Р±РёР»РµС‚Р°С…, РЅР°С‡РёРЅР°СЏ СЃ С‚РµРєСѓС‰РµРіРѕ РјРѕРјРµРЅС‚Р°.

--- twilio_hook.py
+++ twilio_hook.py
@@ -1,26 +1,41 @@
 #coding: utf-8
 
-from flask import Flask, request, redirect
-import twilio.twiml
 import datetime
-from entites import Ticket, Train
+import twilio.twiml
 
+from flask import Flask, request, redirect
+from entities import Ticket, Train
+
+from store import get_store_session
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
-cfg = Config('grazed.conf')
-STORE_DBURI = cfg.get_value('store', 'dburi')
-engine = create_engine(STORE_DBURI or 'sqlite:///:memory:', echo=False)
-Session = sessionmaker(bind=engine)
-
+session = get_store_session()
 
 @app.route("/sms_request", methods=['GET', 'POST'])
 def reply_to_sms():
     body = request.values.get('Body', '')
     resp = twilio.twiml.Response()
 
-    if body == 'hello':
-        resp.sms(u'Hello, world!')
-    return unicode(resp)
+    if 'next' in body.lower():
+        # Отсылаем информацию по всем билетам        
+        try:
+            tickets = session.query(Ticket)\
+                             .filter(Ticket.departure >= datetime.datetime.now())\
+                             .order_by(Ticket.departure)
+            lines = []
+            for t in tickets:
+                resp.sms(u'%s, %s,%i вагон,%i место' % (
+                    t.departure.strftime('%d.%m.%Y %H:%M'),                    
+                    t.train.name,
+                    t.car,
+                    t.seat
+                ))
+            
+            result = '\n'.join(lines)
+        except NoResultFound:
+            result = u'Nothing to see here, move along.'
+    return unicode(resp).encode('utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True)
